90e7c2ff2b6d3224f612329085a331b10151f8c9
andreyvdl <andreyvictordantasleal2016@gmail.com>
2024-06-08 01:49:46 -0300

    +: adding workflow and test

--- project0/caesar_cipher.rb
+++ project0/caesar_cipher.rb
@@ -1,5 +1,10 @@
-def caesar_cipher(str, nbr)
-  result = str.each_char.map do |c|
+require 'minitest/autorun'
+
+def caesar_cipher(text, nbr)
+  raise TypeError, 'Not a string' unless text.is_a?(String)
+  raise TypeError, 'Not a number' unless nbr.is_a?(Integer)
+
+  result = text.each_char.map do |c|
     if c.between?('A', 'Z')
       ((c.ord - 'A'.ord + nbr) % 26 + 'A'.ord).chr
     elsif c.between?('a', 'z')
@@ -9,8 +14,20 @@ def caesar_cipher(str, nbr)
     end
   end.join
 
-  puts result
+  result
 end
 
-# add unit tests later
-caesar_cipher('What a string!', 5)
+class TestCaesar < Minitest::Test
+  def test_valid_inputs
+    assert_equal('Bcde', caesar_cipher('Abcd', 1))
+    assert_equal('Bcde', caesar_cipher('Abcd', 27))
+    assert_equal('hello world!', caesar_cipher('hello world!', 0))
+    assert_equal('ifmmp xpsme!', caesar_cipher('hello world!', 1))
+    assert_equal('b@!#$%&*()~\'\\"A', caesar_cipher('a@!#$%&*()~\'\\"Z', 1))
+  end
+
+  def test_invalid_inputs
+    assert_raises(TypeError) { caesar_cipher(1, 1) }
+    assert_raises(TypeError) { caesar_cipher('hello', 'world') }
+  end
+end
